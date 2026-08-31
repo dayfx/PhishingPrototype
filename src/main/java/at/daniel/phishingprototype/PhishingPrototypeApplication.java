70f9f5ac64c8172e9638ab03dc01211794e11b43
dayfx <if22b206@technikum-wien.at>
2026-08-31 18:38:33 +0200

    first commit

---
+++ src/main/java/at/daniel/phishingprototype/PhishingPrototypeApplication.java
@@ -0,0 +1,13 @@
+package at.daniel.phishingprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhishingPrototypeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhishingPrototypeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/daniel/phishingprototype/PhishingPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package at.daniel.phishingprototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhishingPrototypeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
